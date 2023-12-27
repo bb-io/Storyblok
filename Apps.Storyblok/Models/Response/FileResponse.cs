bb983613a9e481f8f7acd869cbd3112faf45872c
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-12-27 03:57:52 +0200

    New files

--- Apps.Storyblok/Models/Response/FileResponse.cs
+++ Apps.Storyblok/Models/Response/FileResponse.cs
@@ -1,8 +1,8 @@
-using File = Blackbird.Applications.Sdk.Common.Files.File;
+using Blackbird.Applications.Sdk.Common.Files;
 
 namespace Apps.Storyblok.Models.Response;
 
 public class FileResponse
 {
-    public File File { get; set; }
+    public FileReference File { get; set; }
 }
