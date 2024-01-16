b60fa06d886a61b4381198b87d766c2a1964f800
bZverok <137277669+bZverok@users.noreply.github.com>
2024-01-16 15:20:37 +0200

    Improved HTML converter (#15)
    * CI: update workflow to invoke DEV deployment
    
    * provided converting of markdown tables
    
    * Increased version
    
    ---------
    
    Co-authored-by: Ihor Kriatov <ihor.kriatov@blackbird.io>

---
+++ Apps.Storyblok/ContentConverters/Constants/ConverterConstants.cs
@@ -0,0 +1,17 @@
+namespace Apps.Storyblok.ContentConverters.Constants;
+
+public static class ConverterConstants
+{
+    public const string IdAttr = "id";
+    public const string OriginalRichTextAttr = "original";
+    public const string OriginalTableAttr = "original-table";
+    public const string ComponentPath = "path";
+    public const string StyleValueAttr = "style-value";
+    public const string LeadingSpacesAttr = "leading-spaces";
+    public const string TrailingSpacesAttr = "trailing-spaces";
+
+    public const string RichTextId = "richtext";
+    public const string TableId = "table:table";
+    public const string MarkdownTableId = "table:markdownTable";
+    public const string PageIdNode = "page";
+}
---
+++ Apps.Storyblok/ContentConverters/StoryblokToHtmlConverter.cs
@@ -0,0 +1,175 @@
+using System.Text;
+using Apps.Storyblok.ContentConverters.Constants;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Apps.Storyblok.ContentConverters;
+
+public static class StoryblokToHtmlConverter
+{
+    private static readonly IEnumerable<string> SkippableFields = new[] { "language", "url", "text_nodes", "page" };
+
+    private static readonly IEnumerable<string> StyleFields = new[]
+        { "text_color", "background_blur", "background_brightness", "image_layout", "author" };
+
+    public static byte[] ToHtml(string json)
+    {
+        var translatableData = JsonConvert.DeserializeObject<Dictionary<string, string>>(json)!
+            .Where(x => !SkippableFields.Contains(x.Key))
+            .ToList();
+
+        var (doc, bodyNode) = PrepareEmptyHtmlDocument();
+
+        translatableData.ForEach(x =>
+        {
+            var node = doc.CreateElement("div");
+
+            node.SetAttributeValue(ConverterConstants.IdAttr, x.Key);
+            bodyNode.AppendChild(node);
+
+            if (x.Key.Contains(ConverterConstants.RichTextId))
+            {
+                ConvertRichTextToHtml(doc, node, x.Value);
+                return;
+            }
+
+            if (x.Key.EndsWith(ConverterConstants.TableId))
+            {
+                ConvertTableToHtml(doc, node, x.Value);
+                return;
+            }
+
+            if (x.Key.EndsWith(ConverterConstants.MarkdownTableId))
+            {
+                ConvertMarkdownToHtml(doc, node, x.Value);
+                return;
+            }
+
+            if (StyleFields.Any(y => x.Key.EndsWith(y)))
+            {
+                node.SetAttributeValue(ConverterConstants.StyleValueAttr, x.Value);
+                return;
+            }
+
+            node.InnerHtml = x.Value;
+        });
+
+        return Encoding.UTF8.GetBytes(doc.DocumentNode.OuterHtml);
+    }
+
+    private static void ConvertRichTextToHtml(HtmlDocument doc, HtmlNode richTextNode, string richTextJson)
+    {
+        richTextNode.SetAttributeValue(ConverterConstants.OriginalRichTextAttr, richTextJson);
+
+        var richText = JObject.Parse(richTextJson);
+
+        var contentNodes = richText["content"]!
+            .Children()
+            .OfType<JObject>()
+            .ToList();
+
+        var linkNodes = richText
+            .Descendants()
+            .Where(x => (x as JProperty)?.Name == "href" && x.Parent!["linktype"]!.ToString() == "url")
+            .ToList();
+
+        var linkNode = doc.CreateElement("div");
+        richTextNode.AppendChild(linkNode);
+
+        linkNodes.ForEach(x =>
+        {
+            var property = (x as JProperty)!;
+            var node = doc.CreateElement("div");
+
+            node.SetAttributeValue(ConverterConstants.ComponentPath, property.Path);
+            node.InnerHtml = property.First!.Value<string>();
+
+            linkNode.AppendChild(node);
+        });
+
+        contentNodes.ForEach(x =>
+        {
+            var type = x["type"]!.ToString();
+
+            var richTextContentNode = doc.CreateElement(GetHtmlTag(type));
+            richTextNode.AppendChild(richTextContentNode);
+
+            var translatableNodes = x
+                .Descendants()
+                .Where(x => (x as JProperty)?.Name is "text")
+                .ToList();
+
+            translatableNodes.ForEach(x =>
+            {
+                var property = (x as JProperty)!;
+                var node = doc.CreateElement("span");
+
+                node.SetAttributeValue(ConverterConstants.ComponentPath, property.Path);
+                node.InnerHtml = property.First!.Value<string>();
+
+                richTextContentNode.AppendChild(node);
+            });
+        });
+    }
+
+    private static void ConvertTableToHtml(HtmlDocument doc, HtmlNode tableNode, string tableJson)
+    {
+        tableNode.SetAttributeValue(ConverterConstants.OriginalTableAttr, tableJson);
+
+        var table = JObject.Parse(tableJson);
+
+        var valueNodes = table
+            .Descendants()
+            .Where(x => (x as JProperty)?.Name == "value")
+            .ToList();
+
+        valueNodes.ForEach(x =>
+        {
+            var property = (x as JProperty)!;
+            var tableValueNode = doc.CreateElement("div");
+
+            tableValueNode.SetAttributeValue(ConverterConstants.ComponentPath, property.Path);
+            tableValueNode.InnerHtml = property.First!.Value<string>();
+            tableNode.AppendChild(tableValueNode);
+        });
+    }
+
+    private static void ConvertMarkdownToHtml(HtmlDocument doc, HtmlNode node, string markdownTable)
+    {
+        var tableElements = markdownTable.Split("|").SkipLast(1).Skip(1).ToList();
+        tableElements.ForEach(x =>
+        {
+            var tableElementNode = doc.CreateElement("p");
+
+            tableElementNode.SetAttributeValue(ConverterConstants.LeadingSpacesAttr,
+                (x.Length - x.TrimStart(' ').Length).ToString());
+            tableElementNode.SetAttributeValue(ConverterConstants.TrailingSpacesAttr,
+                (x.Length - x.TrimEnd(' ').Length).ToString());
+
+            tableElementNode.InnerHtml = x.Trim(' ');
+            node.AppendChild(tableElementNode);
+        });
+    }
+
+    private static (HtmlDocument document, HtmlNode bodyNode) PrepareEmptyHtmlDocument()
+    {
+        var htmlDoc = new HtmlDocument();
+        var htmlNode = htmlDoc.CreateElement("html");
+        htmlDoc.DocumentNode.AppendChild(htmlNode);
+        htmlNode.AppendChild(htmlDoc.CreateElement("head"));
+
+        var bodyNode = htmlDoc.CreateElement("body");
+        htmlNode.AppendChild(bodyNode);
+
+        return (htmlDoc, bodyNode);
+    }
+
+    private static string GetHtmlTag(string type)
+        => type switch
+        {
+            "heading" => "h1",
+            "paragraph" => "p",
+            _ => "div"
+        };
+}
